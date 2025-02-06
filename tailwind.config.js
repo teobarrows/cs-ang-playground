c08c3c16de83622127210f151804ed4557579d40
Teo Milicevic <teomilicevic@Teos-MacBook-Pro-BCS.local>
2025-02-06 09:27:47 -0500

    a ton

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
 /** @type {import('tailwindcss').Config} */
+import { scopedPreflightStyles, isolateInsideOfContainer } from 'tailwindcss-scoped-preflight';
 module.exports = {
   content: [
     "./src/**/*.{html,ts}",
@@ -6,5 +7,16 @@ module.exports = {
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    // ...
+    scopedPreflightStyles({
+      isolationStrategy: isolateInsideOfContainer('.twp', {
+        except: '.no-twp', // optional, to exclude some elements under .twp from being preflighted, like external markup
+      }),
+    }),
+  ],
+  important: true,
+  corePlugins: {
+    preflight: false, // disable preflight styles
+  },
 }
